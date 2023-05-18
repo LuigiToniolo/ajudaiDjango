ad1e28d29d34008966bac680008734158a5b4924
and270 <andresloureiross@gmail.com>
2023-05-18 20:30:52 -0300

    ajuste no arquivo setting seprarando a parte de código do static que é só para desenvolvimento

--- web_project/settings.py
+++ web_project/settings.py
@@ -128,9 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#PARA DESENVOLVIMENTO
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 #PARA PRODUÇÃO:
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #FIM PARA PRODUÇÃO
