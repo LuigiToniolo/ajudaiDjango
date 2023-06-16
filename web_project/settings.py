56a7e42937f18677fcf4b71269d64b074f4b52db
and270 <andresloureiross@gmail.com>
2023-06-16 19:10:52 -0300

    elimina marca "para desenvolvimento de um comando setting que precisa também estar na produção

--- web_project/settings.py
+++ web_project/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-#PARA DESENVOLVIMENTO
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 #PARA PRODUÇÃO:
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
