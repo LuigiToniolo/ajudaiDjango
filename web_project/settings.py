7da1cea1d19c1b66b8e60d2a9aaa903f72047906
and270 <andresloureiross@gmail.com>
2023-06-17 11:32:14 -0300

    corrige registro de método de pagamento no webhook. Adiciona registro de método de pagaemento no admin. AJusta comentário em settings

--- web_project/settings.py
+++ web_project/settings.py
@@ -129,8 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+#PARA QUANDO HOUVER VARIAS PASTAS E SUBPASATAS DENTRO DO STATIC (??)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-#PARA PRODUÇÃO:
+#PARA UM UNICO ROOT:
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #FIM PARA PRODUÇÃO
 
