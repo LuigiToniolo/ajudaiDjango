7a9dbde7ef2c926f6973982987e653c7fefaee88
SeaSkyThe <marceloer2011@gmail.com>
2023-12-22 15:20:57 -0300

    feat: changing django_q settings

--- web_project/settings.py
+++ web_project/settings.py
@@ -193,10 +193,10 @@ if IS_DEVELOPMENT_ENV:
 # DJANGO - Q config
 Q_CLUSTER = {
     "name": "DjangORM",
-    "workers": 4,
+    #"workers": 4,
     "timeout": 90,
     "retry": 120,
     "queue_limit": 50,
-    "bulk": 10,
+    "bulk": 7,
     "orm": "default",  # Use Django's ORM + database as the broker
 }
