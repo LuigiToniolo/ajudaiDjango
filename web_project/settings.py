15f20c472b1b7e05c12304c3a70801b51143cfd0
and270 <andresloureiross@gmail.com>
2025-12-09 14:01:42 -0300

    feat: add MySQL support in database settings with environment variable configuration

--- web_project/settings.py
+++ web_project/settings.py
@@ -94,17 +94,37 @@ WSGI_APPLICATION = "web_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        "OPTIONS": {
-            "timeout": 30,  # Wait up to 30 seconds for locks to clear
-        },
+
+USE_MYSQL = env.bool("USE_MYSQL", default=False)
+
+if USE_MYSQL:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": env("MYSQL_DB_NAME"),
+            "USER": env("MYSQL_DB_USER"),
+            "PASSWORD": env("MYSQL_DB_PASSWORD"),
+            "HOST": env("MYSQL_DB_HOST", default="localhost"),
+            "PORT": env("MYSQL_DB_PORT", default="3306"),
+            "OPTIONS": {
+                "init_command": "SET sql_mode='STRICT_ALL_TABLES';",
+            },
+        }
     }
-}
-# Don't keep connections open between requests (helps SQLite concurrency)
-CONN_MAX_AGE = 0
+    # For MySQL it's OK to keep connections open a bit
+    CONN_MAX_AGE = 60
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+            "OPTIONS": {
+                "timeout": 30,  # Wait up to 30 seconds for locks to clear
+            },
+        }
+    }
+    # Don't keep connections open between requests (helps SQLite concurrency)
+    CONN_MAX_AGE = 0
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
